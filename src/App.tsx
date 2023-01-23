a83a89e9449155852078c4196635396223c6ff52
Sedrak Adoyan <set.adoyan@gmail.com>
2023-01-23 15:29:32 +0400

    feat: add garage-context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from "react";
+import { FC, useState } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -7,13 +7,41 @@ import Winners from "./pages/Winners";
 
 import { Header } from "./components/Header";
 
+import { ICar } from "./typings/ICar";
+import { IGarageContext } from "./typings/IGarage";
+
 const App: FC = () => {
+  const [page, setPage] = useState<number>(1);
+  const [limit, setLimit] = useState<number>(7);
+  const [count, setCount] = useState<number>(0);
+  const [cars, setCars] = useState<ICar[]>([]);
+  const [selectedCar, setSelectedCar] = useState<ICar>({ name: "", color: "" });
+  const [newCar, setNewCar] = useState<ICar>({ name: "", color: "#000000" });
+  const [isRace, setIsRace] = useState<boolean>(false);
+
+  const GarageContext: IGarageContext = {
+    page,
+    setPage,
+    limit,
+    setLimit,
+    count,
+    setCount,
+    cars,
+    setCars,
+    selectedCar,
+    setSelectedCar,
+    newCar,
+    setNewCar,
+    isRace,
+    setIsRace,
+  };
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/winners" element={<Winners />} />
-        <Route path="*" element={<Garage />} />
+        <Route path="*" element={<Garage context={GarageContext} />} />
       </Routes>
     </BrowserRouter>
   );

40c22157cd575f77bdc8ee79f87b94558fad91c8
Sedrak Adoyan <set.adoyan@gmail.com>
2023-01-24 02:29:35 +0400

    fix: fix car reset btn

--- src/components/Race/Race.tsx
+++ src/components/Race/Race.tsx
@@ -17,7 +17,7 @@ export const Race: FC<IRaceProps> = ({ isStarted, onStart, onReset }) => {
       <button className={styles.btn} onClick={handleStart} disabled={isStarted}>
         Start
       </button>
-      <button className={styles.btn} onClick={handleReset} disabled={isStarted}>
+      <button className={styles.btn} onClick={handleReset}>
         Reset
       </button>
     </div>
